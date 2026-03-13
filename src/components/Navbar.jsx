07f7621bd5a12d9649e8eafe9fc6205ff4ccf567
Muhammad Umar Attique <umarmalik3120225@gmail.com>
2026-03-13 21:35:23 +0500

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -58,7 +58,7 @@ const Navbar = () => {
             <img src="/logo.avif" alt="Switzerland IPTV Logo" width="50" height="50" decoding="async" style={{ width: '100%', height: '100%', objectFit: 'cover' }} />
           </div>
           <span style={{ fontSize: '1.5rem', fontWeight: '800', background: 'var(--success-gradient)', WebkitBackgroundClip: 'text', WebkitTextFillColor: 'transparent' }}>
-            IPTV Suisse
+            IPTV Switzerland
           </span>
         </Link>
 
